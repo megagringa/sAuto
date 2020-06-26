82ee4e06694032cf4c7c6830c4cbbbad79781096
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2020-06-26 18:39:35 -0300

    Formulario agregando datos...

--- js/app.js
+++ js/app.js
@@ -1,8 +1,46 @@
 //Cotizador Constructor
-
-function Seguro(){
+//Constructor para seguro
+function Seguro(marca, anio, tipo){
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo;
 
 }
+
+//Todo lo que se muestra
+function Interfaz(){
+
+}
+
+//EventListener
+const formulario = document.getElementById('cotizar-seguro');
+
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const marca = document.getElementById('marca');
+    const marcaSeleccionada = marca.option[marca.selectedIndex].value;
+
+    //Leer el año seleccionado del <select>
+    const anio = document.getElementById('anio');
+    const anioSeleccionado = anio.option[anio.selectedIndex].value;
+
+    //Lee el valor del radio button
+    const tipo = document.querySelector('input[name="tipo"]:checked').value
+
+    //Crear instancia de Interfaz
+    const interfaz = new Interfaz();
+
+    //Revisamos que los campos no esten vacios
+    if(marcaSeleccionada === '' || anioSeleccionado === '' || tipo === ''){
+        //Interfaz imprimiendo un error
+        console.log('Faltan Datos');
+    }else{
+        //Instanciar seguro y mostrar intefaz
+        console.log('Todo Correcto');
+    }
+
+})
 
 
 const max = new Date().getFullYear();
